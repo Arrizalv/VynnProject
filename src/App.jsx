6a81d098607cc0c9db0a0054e23445a02afb6f18
Arrizalv <arrizalalvin27@gmail.com>
2025-10-13 17:16:52 +0700

    SunAftn

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import Header from "./Header"
 import Footer from "./Footer"
 import Card from "./Card"
+import Master from "./Master";
 
 function App() {
   return(
     <>
     <Header/>
+    <Master/>
     <Card/>
     <Card/>
     <Card/>
---
+++ src/Master.jsx
@@ -0,0 +1,65 @@
+
+function Master() {
+    return(
+           <>
+           <article id="Home">
+            <div class="SubHead1">
+                <h2>
+                    Hi, I'm Arrizal Alvin a.k.a Front-end Developer
+                </h2>
+                <p>
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                    Deleniti ut adipisci aliquid soluta provident cum incidunt
+                    cupiditate amet quibusdam suscipit!
+                </p>
+            </div>
+            <a>More About Me</a>   
+        </article>
+        <article id="AboutMe">
+            <h2>About Me</h2>
+            <h3>Personal Info</h3>
+            <ul class="info">
+                <li>First Name: Arrizal</li>
+                <li>Age: 20</li>
+                <li>Freelance: Available</li>
+                <li>Phone: +6282281167644</li>
+                <li>Instagram: @arrizalalvin_fa</li>
+            </ul>
+            <ul class="info">
+                <li>Last Name: Fathurrohman</li>
+                <li>Nationallity: Indonesian</li>
+                <li>Address: Indonesia</li>
+                <li>Email: arrizalalvin@gmail.com</li>
+                <li>Language: Indonesian, English</li>
+            </ul>
+            <section>
+                <h4>12+ Years of Experience</h4>
+                <h4>97+ Happy Customers</h4>
+                <h4>23+ Completed Projects</h4>
+                <h4>31+ Awards Won</h4>
+            </section>
+            <a>Download CV</a>
+        </article>
+            <h1>My Skills</h1>
+        <article id="Skill">
+            <div id='ProgressBar'>
+            <h5>30% HTML</h5>
+            <progress id='data' value='30' max='100'></progress>
+            <h5>10% CSS</h5>
+            <progress id='data' value='10' max='100'></progress>
+            <h5>5% Javascript</h5>
+            <progress id='data' value='5' max='100'></progress>
+            </div>
+            <div id='ProgressBar'>
+            <h5>40% Python</h5>
+            <progress id='data' value='40' max='100'></progress>
+            <h5>40% Photoshop</h5>
+            <progress id='data' value='40' max='100'></progress>
+            <h5>35% Canva</h5>
+            <progress id='data' value='35' max='100'></progress>
+            </div>
+        </article>
+        </>
+    );
+        }
+export default Master
